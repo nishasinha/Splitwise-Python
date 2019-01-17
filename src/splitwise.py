4363de5af8e867d5ea2fff9f331c2b4ecee066cc
Nisha Kumari <nishasinha68@gmail.com>
2019-01-17 12:11:57 +0530

    Refactoring code.

--- src/splitwise.py
+++ src/splitwise.py
@@ -3,55 +3,46 @@ class Splitwise:
         self.members = members
         self.expenses = expenses
 
-    def find_per_member_share(self):
-        num_of_members = len(self.members)
-        sum_of_expenses = 0.0
-        for expense in self.expenses:
-            sum_of_expenses += expense[1]
-        return sum_of_expenses/num_of_members
+    def calculate_per_member_share(self):
+        sum_of_expenses = float(sum([x[1] for x in self.expenses]))
+        return sum_of_expenses/len(self.members)
 
-    def find_balances_of_every_member(self):
-        balances = []
-        each_share = self.find_per_member_share()
+    def calculate_per_member_balances(self):
+        each_share = self.calculate_per_member_share()
+        balances = {}
         for member in self.members:
-            balances.append([member, round(self.get_expense_of_member(member) - each_share, 2)])
+            expense_of_member = sum([x[1] for x in self.expenses if x[0] == member])
+            balances[member] = round(expense_of_member - each_share, 2)
         return balances
 
-    def get_expense_of_member(self, member):
-        m_expense = 0
-        for expense in self.expenses:
-            if expense[0] == member:
-                m_expense += expense[1]
-        return m_expense
-
     def calculate_borrowers_and_lenders_balances(self):
-        lenders = []
-        borrowers = []
-        member_balances = self.find_balances_of_every_member()
-        for member_balance in member_balances:
-            if member_balance[1] > 0:
-                lenders.append(member_balance)
-            elif member_balance[1] < 0:
-                borrowers.append(member_balance)
+        lenders = {}
+        borrowers = {}
+        member_balances = self.calculate_per_member_balances()
+        for member, balance in member_balances.items():
+            if balance > 0:
+                lenders[member] = balance
+            elif balance < 0:
+                borrowers[member] = balance
 
         return borrowers, lenders
 
-    def pay_borrowed_amount_to_lender(self, borrowed_amt, borrower, lender):
-        lenders_amt = lender[1]
+    def pay_borrowed_amount_to_lender(self, borrowed_amt, borrowers, borrower, lenders, lender):
+        lenders_amt = lenders[lender]
         amount_to_pay = lenders_amt if borrowed_amt > lenders_amt else borrowed_amt
 
-        borrower[1] += amount_to_pay
-        lender[1] -= amount_to_pay
-        borrowed_amt = borrowed_amt - amount_to_pay
-        return [borrower[0], lender[0], amount_to_pay], borrowed_amt
+        borrowers[borrower] += amount_to_pay
+        lenders[lender] -= amount_to_pay
+        borrowed_amt -= amount_to_pay
+        return [borrower, lender, amount_to_pay], borrowed_amt
 
     def split_expense(self):
         (borrowers, lenders) = self.calculate_borrowers_and_lenders_balances()
         transactions = []
-        for borrower in borrowers:
-            borrowed_amt = round(abs(borrower[1]), 2)
-            while borrowed_amt > 0:
-                lender = sorted(lenders, key=lambda x: x[1], reverse=True)[0]
-                transaction, borrowed_amt = self.pay_borrowed_amount_to_lender(borrowed_amt, borrower, lender)
+        for borrower, borrowed_amt in borrowers.items():
+            abs_borrowed_amt = round(abs(borrowed_amt), 2)
+            while abs_borrowed_amt > 0:
+                lender = max(lenders, key=lenders.get)
+                transaction, abs_borrowed_amt = self.pay_borrowed_amount_to_lender(abs_borrowed_amt, borrowers, borrower, lenders, lender)
                 transactions.append(transaction)
         return transactions
--- tests/splitwise_test.py
+++ tests/splitwise_test.py
@@ -3,37 +3,29 @@ from src.splitwise import Splitwise
 
 class TestSplitwise(unittest.TestCase):
 
-    def test_find_per_member_share(self):
+    def test_calculate_per_member_share(self):
         members = ['A', 'B']
         expenses = [('A', 300, 'snacks'), ('B', 100, 'tickets')]
         splitwise = Splitwise(members, expenses)
-        self.assertEquals(200, splitwise.find_per_member_share())
+        self.assertEquals(200, splitwise.calculate_per_member_share())
 
-    def test_get_expense_of_member(self):
-        members = ['A', 'B', 'C']
-        expenses = [('A', 200, 'snacks'), ('B', 100, 'tickets'), ('B', 200, 'tickets_1')]
-        splitwise = Splitwise(members, expenses)
-        self.assertEquals(200, splitwise.get_expense_of_member('A'))
-        self.assertEquals(300, splitwise.get_expense_of_member('B'))
-        self.assertEquals(0, splitwise.get_expense_of_member('C'))
-
-    def test_get_all_balances(self):
+    def test_calculate_per_member_balances(self):
         members = ['A', 'B', 'C']
         expenses = [('A', 200, 'snacks'), ('B', 100, 'tickets')]
         splitwise = Splitwise(members, expenses)
-        self.assertEquals(100, splitwise.find_per_member_share())
-        expected = [['A', 100], ['B', 0.0], ['C', -100.0]]
-        self.assertEquals(expected, splitwise.find_balances_of_every_member())
+
+        expected = {'A': 100, 'B': 0.0, 'C': -100.0}
+        self.assertEquals(expected, splitwise.calculate_per_member_balances())
 
     def test_get_borrowers_and_lenders_and_balanced(self):
         members = ['A', 'B', 'C']
         expenses = [('A', 200, 'snacks'), ('B', 100, 'tickets')]
         splitwise = Splitwise(members, expenses)
 
-        borrowers = [['C', -100.0]]
-        lenders = [['A', 100]]
-        self.assertListEqual(borrowers, splitwise.calculate_borrowers_and_lenders_balances()[0])
-        self.assertListEqual(lenders, splitwise.calculate_borrowers_and_lenders_balances()[1])
+        borrowers = {'C': -100.0}
+        lenders = {'A': 100}
+        self.assertDictEqual(borrowers, splitwise.calculate_borrowers_and_lenders_balances()[0])
+        self.assertDictEqual(lenders, splitwise.calculate_borrowers_and_lenders_balances()[1])
 
     def test_split_expense_for_one_borrower_one_lender(self):
         members = ['A', 'B']
@@ -48,16 +40,16 @@ class TestSplitwise(unittest.TestCase):
         expenses = [('A', 300, 'snacks')]
         splitwise = Splitwise(members, expenses)
 
-        expected = [['B', 'A', 100], ['C', 'A', 100]]
-        self.assertListEqual(expected, splitwise.split_expense())
+        expected = [['B', 'A', 100.0], ['C', 'A', 100.0]]
+        self.assertItemsEqual(expected, splitwise.split_expense())
 
     def test_split_expense_for_one_lender_repeated_lends_many_borrowers(self):
         members = ['A', 'B', 'C']
         expenses = [('A', 450, 'snacks'), ('A', 450, 'snacks')]
         splitwise = Splitwise(members, expenses)
 
-        expected = [['B', 'A', 300], ['C', 'A', 300]]
-        self.assertListEqual(expected, splitwise.split_expense())
+        expected = [['B', 'A', 300.0], ['C', 'A', 300.0]]
+        self.assertItemsEqual(expected, splitwise.split_expense())
 
     def test_split_expense_for_many_lenders_one_borrower(self):
         members = ['A', 'B', 'C']
@@ -80,16 +72,16 @@ class TestSplitwise(unittest.TestCase):
         expenses = [('A', 450, 'snacks'), ('B', 150, 'snacks')]
         splitwise = Splitwise(members, expenses)
 
-        expected = [['B', 'A', 50], ['C', 'A', 200]]
-        self.assertListEqual(expected, splitwise.split_expense())
+        expected = [['B', 'A', 50.0], ['C', 'A', 200.0]]
+        self.assertItemsEqual(expected, splitwise.split_expense())
 
     def test_split_expense_for_many_lenders_repeated_lends_many_borrowers(self):
         members = ['A', 'B', 'C']
         expenses = [('A', 500, 'snacks'), ('A', 200, 'snacks_1'), ('B', 150, 'snacks'), ('B', 50, 'snacks_1')]
         splitwise = Splitwise(members, expenses)
 
-        expected = [['B', 'A', 100], ['C', 'A', 300]]
-        self.assertListEqual(expected, splitwise.split_expense())
+        expected = [['B', 'A', 100.0], ['C', 'A', 300.0]]
+        self.assertItemsEqual(expected, splitwise.split_expense())
 
     def test_split_expense_for_many_lenders_repeated_lends_many_borrowers_floats(self):
         members = ['A', 'B', 'C']
@@ -97,7 +89,7 @@ class TestSplitwise(unittest.TestCase):
         splitwise = Splitwise(members, expenses)
 
         expected = [['B', 'A', round(1000/3.0 - 300, 2)], ['C', 'A', round(1000/3.0, 2)]]
-        self.assertListEqual(expected, splitwise.split_expense())
+        self.assertItemsEqual(expected, splitwise.split_expense())
 
 
 if __name__ == '__main__':
